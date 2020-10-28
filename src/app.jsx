24c9b3d458ed198ef2653571ce32298f9b1948c5
Dustin Morris <dustinnuc@gmail.com>
2020-10-27 22:56:24 -0700

    Random Tested.  Some UI improvements

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Wheel from './wheel';
 import styled from 'styled-components';
+import regeneratorRuntime from "regenerator-runtime";
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      names: ['test1', 'test2', 'test3', 'test4']
+      names: ['Abel', 'Aliona', 'Apekshya', 'Bradley', 'Candace', 'Chris P', 'Chris R', 'Cody', 'Collin', 'Debbie', 'Dustin','Edgar', 'Gina', 'Jason', 'Kelson', 'Malik', 'Matthew', 'Mitchell', 'Nick', 'Paul', 'Randall', 'Ty'],
+      currentNames: [
+        ['Abel', 1], ['Aliona',1], ['Apekshya',1], ['Bradley',1], ['Candace',1], ['Chris P',1], ['Chris R',1], ['Cody',1], ['Collin',1], ['Debbie',1], ['Dustin',1],['Edgar',1], ['Gina',1], ['Jason',1], ['Kelson',1], ['Malik',1], ['Matthew',1],
+        ['Mitchell',1],
+        ['Nick',1],
+        ['Paul',1],
+        ['Randall',1], ['Ty',1]],
+      test: {},
     }
     this.handleSpinClick = this.handleSpinClick.bind(this);
     this.shiftNames = this.shiftNames.bind(this);
@@ -14,32 +22,48 @@ class App extends React.Component {
 
   handleSpinClick(e){
     e.preventDefault();
+    let count = 0;
     const maxNames = this.state.names.length;
     const shiftBy = Math.floor((Math.random()*maxNames));
-    let rotations = Math.floor((Math.random()*500*maxNames)+1);
-    while(rotations >= 0){
-      console.log(rotations);
-      setTimeout(() => {
-        this.shiftNames();
-      }, 1000);
-      rotations--;
+    let rotations = Math.floor((Math.random()*50*maxNames)+1);
+    const gen = this.shiftNames(rotations);
+    let first;
+    for(let i=0 ; i < rotations; i++){
+      gen.next();
     }
+    first = this.state.currentNames[0];
+    if(first[1] !== 1){
+      const probability = first[1];
+      const spinAgain = Math.floor(Math.random()*20*probability) > 7 ? true : false;
+      if(spinAgain){
+        for(let i=0; i < rotations; i++){
+          gen.next();
+        }
+      }
+    }
+    let currentNames = this.state.currentNames;
+    currentNames[0][1] *= .5;
   }
 
-  shiftNames(){
-    let newNameOrder = this.state.names;
-    let first = newNameOrder.pop();
-    newNameOrder.unshift(first);
-    this.setState({
-      names: newNameOrder
-    });
+  *shiftNames(rotations){
+      let newNameOrder = this.state.currentNames;
+      let first;
+      while(rotations >= 0){
+        first = newNameOrder.pop();
+        newNameOrder.unshift(first);
+        this.setState({
+          currentNames: newNameOrder
+        });
+        rotations--;
+      }
+    yield first;
   }
 
   render() {
     return (
       <>
         <button onClick={this.handleSpinClick}>Spin</button>
-        <Wheel names={this.state.names}></Wheel>
+        <Wheel names={this.state.currentNames}></Wheel>
         <Axel></Axel>
       </>
     )
--- src/wheel.jsx
+++ src/wheel.jsx
@@ -16,7 +16,7 @@ function Wheel({names}) {
         if(i === l-1){
           divStyle['color'] = 'red';
         }
-        return (<Icon id={name} style={divStyle} key={name+i}>{name}</Icon>);
+        return (<Icon id={name[0]} style={divStyle} key={name[0]+i}>{name[0]}</Icon>);
       })
       }
       </Circle>
@@ -28,8 +28,8 @@ const PopUp = styled.div`
   position: fixed;
   border-radius: 50%;
   background-color: white;
-  width: calc(60vh);
-  height: calc(60vh);
+  width: calc(75vh);
+  height: calc(75vh);
   top: 50%;
   left: 50%;
   bottom: auto;
@@ -40,36 +40,37 @@ const PopUp = styled.div`
   box-shadow: 0 0 8px 0 rgba(0,0,0,.35);
 `;
 const Circle = styled.div`
-  width: 250px;
-  height: 250px;
+  width: 400px;
+  height: 400px;
   opacity: 1;
 `;
-const rotate = keyframes`
+const pulse = keyframes`
   from {
     transform: scale(1);
   }
 
   50% {
-    transform: scale(1.1);
+    transform: scale(1.5);
   }
 
   100% {
     transform: scale(1);
   }
 `;
+
 const Icon = styled.div`
     color: black;
     display: block;
-    height: 35px;
-    width: 35px;
+    height: 10px;
+    width: 30px;
     line-height: 20px;
-    margin-left: -10px;
-    margin-top: -10px;
+    margin-left: -30px;
+    margin-top: -30px;
     position: absolute;
     text-align: center;
 
   &:hover {
-    animation: ${rotate} 1s linear;
+    animation: ${pulse} 1s linear;
     animation-iteration-count: 1;
   }`;
 
