daf3400916f0dd3d4531225b6cf7962df5049f25
dustinmorris1 <dustinnuc@gmail.com>
2020-11-18 23:10:17 -0800

    tweaked color gradient

--- src/dashboard/wheel.jsx
+++ src/dashboard/wheel.jsx
@@ -4,10 +4,18 @@ import styled, { keyframes } from 'styled-components';
 function Wheel({names}) {
   const l = names.length;
   const colors = ['red', 'yellow', 'green', 'blue'];
-  generateWheelGradient(l, colors);
+  const wheelGradient = generateWheelGradient(l, colors);
+  let colorWheelStyle = {
+    position: 'fixed',
+    height: '75vh',
+    width: '75vh',
+    background: 'white',
+    background: `conic-gradient(${wheelGradient})`,
+  };
    return (
      <PopUp>
       <Circle>
+      <ColorWheel style={colorWheelStyle}></ColorWheel>
       { names.map((name,i)=>{
         let left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
         let top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
@@ -16,21 +24,16 @@ function Wheel({names}) {
         top: top,
         };
         if(i === 0){
-          divStyle['color'] = 'red';
+          divStyle['color'] = 'purple';
         }
-        const rotation = (-90 + (360/l * i)).toString() + "deg";
+        const rotation = (-90 + (360/l) * i).toString() + "deg";
         let rotateStyle = {
           "transform": `rotate(${rotation})`,
         };
-        let colorWheelStyle = {
-          height: '75vh',
-          width: '75vh',
-          background: 'white',
-          background: `conic-gradient(${wheelGradient})`,
-        }
       
-        return (<div><div><ColorWheel style={colorWheelStyle}></ColorWheel></div><Icon id={name[0]} style={divStyle} key={name[0]+i}>
-          <Name style={rotateStyle}>{name[0]}</Name></Icon></div>);
+      
+        return (<Icon id={name[0]} style={divStyle} key={name[0]+i}>
+          <Name style={rotateStyle}>{name[0]}</Name></Icon>);
       })
       }
       </Circle>
@@ -40,17 +43,21 @@ function Wheel({names}) {
 }
 
 const generateWheelGradient = (numberOfWedges, colors) => {
-  const percentageOfWheel = 360/numberOfWedges;
-  let result = [];
-  for(let i = 0; i < numberOfWedges; i++) {
-    result.push(colors[i%colors.length]);
-    result.push(' ' + percentageOfWheel + '%');
-    if(i !== numberOfWedges - 1){
-      result.push(',');
-    }
+  const percentageOfWheel = (100/numberOfWedges).toPrecision(4);
+  let result = [colors[0] + ' ' + percentageOfWheel + '%'];
+  for(let i = 1; i < numberOfWedges; i++) {
+    result.push(
+      colors[i%colors.length] + ' ' + percentageOfWheel*(i).toPrecision(4) + '%' + "," +
+      colors[i%colors.length] + ' ' + percentageOfWheel*(i+1).toPrecision(4) + '%'                          
+    );
   }
-  return result;
+  console.log(result.join(","));
+  return result.join(",");
 };
+
+const ColorWheel = styled.div`
+  border-radius: 50%;
+`;
 
 const Name = styled.div`
   font-size: 30px;
@@ -94,7 +101,7 @@ const Icon = styled.div`
     color: black;
     display: block;
     height: 10px;
-    width: 30px;
+    width: 105px;
     line-height: 20px;
     margin-left: -30px;
     margin-top: -30px;
